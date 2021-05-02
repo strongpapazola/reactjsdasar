c8f983d010616ad10000c94fccbd762902d3f1c1
Your Name <you@example.com>
2021-05-02 13:45:40 +0700

    up

---
+++ myapp/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  Form, Button, Card, Navbar, Nav, NavDropdown, FormControl
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends React.Component {
+  render() {
+    const html = (
+      <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <div class="container mt-3">
+      <Card>
+        <Card.Header>Login</Card.Header>
+        <Card.Body>
+
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="info" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+        </Card.Body>
+      </Card>
+      </div>
+      </div>
+    )
+
+    return(
+      html
+    )
+  }
+}
+
+export default App;
